6eef3b5b51a2608470961015238f8c8ef34ade90
abilcode <m.nailfad@gmail.com>
2023-01-14 14:25:01 +0700

    update data

--- code/data_appending.py
+++ code/data_appending.py
@@ -2,9 +2,18 @@ import os
 import pandas as pd
 import numpy as np
 
-def data_appending():
-    print('start appending data')
-
-
 if __name__ == "__main__":
     print('Start!')
+    dir_ = '../weather-forecasting-datavidia/data'
+    for dirpath, dirname, filename in os.walk(dir_+'/merged'):
+        print(f"There are {len(dirname)} directories and {len(filename)} file in '{dirpath}'.")
+    a,b,c,d,e,f,g,h,i,j = np.zeros(10)
+    arr_con = [a,b,c,d,e,f,g,h,i,j]
+    entries = os.listdir(dir_+'/merged')
+    entries.sort()
+    for i in range(10):
+        arr_con[i] = pd.read_csv(f'{dir_}/merged/{entries[i]}')
+    appended_data = pd.concat(arr_con, axis=0)
+    appended_data.to_csv('../weather-forecasting-datavidia/appended_data.csv')
+
+
